0a27816683723b72f9f5d4e0c3824cf4426da66d
UsaNews65 <139873073+UsaNews65@users.noreply.github.com>
2023-07-18 22:39:05 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`http://newsupdates.42web.io/` + encodeURI(path as string)
 				}`,
 			},
 		};
